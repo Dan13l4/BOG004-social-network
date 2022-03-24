e7404917400197191cc54c5c2463e38f44bb58e5
Dan13l4 <kdbaron426@gmail.com>
2022-03-24 12:12:25 -0500

    Dandole estilo a la pagina de publicacion mobile fisrt

--- src/view/board.js
+++ src/view/board.js
@@ -2,17 +2,21 @@ import { close, recet } from '../lib/index.js';
 
 export default () => {
   const board = `
+  <div class= 'close'>
+    <button type='submit' class='btnClose' id='btnClose'>Cerrar Sesión</button>
+  </div>
   <section class='board'>
     <div>
       <img class='imagen' src=''></img>
     </div>
     <div class= 'containerMuro'>
-      <input type='text' placeholder='Comparte tus experiencias...' id='muro'>
-      <button type='submit' class='btnMuro' id='btnMuro'>Publicar</button>
-      <button type='submit' class='btnClose' id='btnClose'>Cerrar Sesión</button>
-    <section>
-    </section>
+      <input type='text' maxlength='150' placeholder='Comparte tus experiencias con un maximo de 150 palabras.' id='muro' class='wall'>
+      <div class= 'botones'>
+        <button type='submit' class='btnMuro' id='btnMuro'>Publicar</button>
+      </div>
     </div>
+  </section>
+  <section id='wallPost' class='pts'>
   </section>`;
 
   const divElem = document.createElement('div');
