bcecdfed2e0512d9e02273154cf4c413873017fb
Dan13l4 <kdbaron426@gmail.com>
2022-03-28 10:55:25 -0500

    Arreglando y eliminando cosas del postWall js

--- src/view/postWall.js
+++ src/view/postWall.js
@@ -1,6 +1,3 @@
-/* eslint-disable */
-import { auth } from '../lib/index.js';
-
 export const look = (box) => {
   const postNewPage = document.querySelector('#wallPost');
   postNewPage.innerHTML = '';
@@ -8,25 +5,14 @@ export const look = (box) => {
     const carry = `
         <div class='watchPost'>
           <div class='conteinerUser'>
-              <p><img class="userpost" src='./img/usuario.png'/>${rest.task.data.name === null ? 'Desconocido' : rest.task.data.name}</p>
+              <p><img class="userpost" src='./img/usuario.png'/>${rest.task.data.name}</p>
           </div>
           <textarea class='nameLook' id='postLook'  readonly>${rest.task.data.publicacion}</textarea>
           <div class='contentButtons'>
                <p class='number' id='counter-likes'> ${rest.task.data.numberLike} me gusta</p>
                
                 `;
-    const carryTwo = `
-             `;
-    let carryThree = '';
-    if (rest.task.data.userId === auth.currentUser.uid) {
-      carryThree = `
-              <div class='buttonsContent'>
-
-              </div>
-      </div>`;
-    }
-    postNewPage.innerHTML += carry + carryThree + carryTwo;
+    postNewPage.innerHTML += carry;
   };
   box.forEach(lookConten);
 };
-/* eslint-enable */
